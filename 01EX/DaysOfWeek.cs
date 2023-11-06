8ff55c21bac2ff5375d7a7dd964d6543505515e4
TeodorStnv <teodor.stnv@gmail.com>
2023-11-06 16:02:19 +0200

    Add project files.

---
+++ 01EX/DaysOfWeek.cs
@@ -0,0 +1,21 @@
+﻿string[] dayOfWeek = {"Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday"
+};
+
+{
+    int day = int.Parse(Console.ReadLine());
+    if (day >= 1 && day <= 7)
+    {
+        Console.WriteLine(dayOfWeek[day - 1]);
+    }
+
+    else
+    {
+        Console.WriteLine("Invalid day!");
+    }
+}
---
+++ 05. Equal Arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] digits = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int[] digits2 = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+bool isEqual = true;
+
+if (digits.Length != digits2.Length)
+{
+    isEqual = false;
+}
+else
+{
+    for (int i = 0; i < digits.Length; i++)
+    {
+        if (digits[i] != digits2[i])
+        {
+            isEqual = false;
+            break;
+        }
+    }
+}
+
+if (isEqual)
+{
+    Console.WriteLine("Arrays are identical.");
+}
+else
+{
+    Console.WriteLine("Arrays are not identical.");
+}
+
